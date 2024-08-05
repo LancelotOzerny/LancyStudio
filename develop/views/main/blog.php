7a7907a8cf8f86b26a3f4e13afcd9092dba2d3ee
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-08-05 20:32:19 +0300

    Добавил заголовки к главным страницам

--- develop/views/main/blog.php
+++ develop/views/main/blog.php
@@ -1,1 +1,11 @@
-<?php
+<div class="page-block border-bottom">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 pb-4">
+                <div class="page-title-wrapper">
+                    <p class="page-title">Блог</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ develop/views/main/certificates.php
@@ -0,0 +1,11 @@
+<div class="page-block border-bottom">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 pb-4">
+                <div class="page-title-wrapper">
+                    <p class="page-title">Сертификаты</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
