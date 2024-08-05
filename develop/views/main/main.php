22c3dd9cba95f56d40508d8548648e5ab816fa5a
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-08-05 03:32:40 +0300

    Добавил представления других страниц и переместил их в директорию main

--- develop/views/main/main.php
+++ develop/views/main/main.php
@@ -1,33 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-
-    <link rel="stylesheet" href="assets/css/bootstrap-4.4.1.css">
-    <link rel="stylesheet" href="assets/css/style.css">
-</head>
-<body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom position-sticky w-100">
-    <div class="container d-flex justify-content-between">
-        <a class="navbar-brand" href="#">
-            <img src="assets/images/logo.png" alt="Lancy" width="64">
-        </a>
-        <div class="collapse navbar-collapse">
-            <div class="navbar-nav">
-                <a class="nav-link active" aria-current="page" href="#">Главная</a>
-                <a class="nav-link" href="#">Портфолио</a>
-                <a class="nav-link" href="#">Сертификаты</a>
-                <a class="nav-link" href="#">Блог</a>
-            </div>
-        </div>
-        <a class="profile d-flex align-items-center text-dark" href="#">
-            <div class="mr-2">Вход/Регистрация</div>
-            <img src="assets/images/empty_profile_icon.png" alt="profile" width="64">
-        </a>
-    </div>
-</nav>
-
 <div class="page-block border-bottom">
     <div class="container">
         <div class="row">
@@ -40,13 +10,13 @@
                 </div>
             </div>
         </div>
-        <div class="row pt-md-5 flex-lg-row-reverse">
-            <div class="col-12 col-lg-4 mb-5 mb-lg-0 d-flex align-items-center">
-                <div class="d-flex justify-content-center">
+        <div class="row pt-md-5">
+            <div class="col-12 mb-5 d-flex align-items-center justify-content-center">
+                <div class="d-flex">
                     <img class="rounded-circle" style="max-width: 100%" src="assets/images/profile.png" alt="Profile Image">
                 </div>
             </div>
-            <div class="col-12 col-lg-8 text-indent-25 text-justify">
+            <div class="col-12 text-indent-25 text-justify">
                 <p>В далеком 2002-м году в Липецкой области родился суперспособный парень, получивший при
                     рождении имя Максим. Уже с пеленок он показал невероятную тягу к знаниям. Пока другие дети дни
                     напролет катались на горках и бегали по улицам, он залпом читал одну книгу
@@ -66,8 +36,3 @@
         </div>
     </div>
 </div>
-
-<script src="assets/js/bootstrap.js"></script>
-<script src="assets/js/jQuery-3.7.1.js"></script>
-</body>
-</html>
