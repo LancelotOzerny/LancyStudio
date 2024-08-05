51f8ad450e13e67d86e912200b3ac3b59e53502b
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-08-05 11:27:57 +0300

    добавил раздел настроек административной панели

--- develop/views/admin/main.php
+++ develop/views/admin/main.php
@@ -1,5 +1,20 @@
 <div class="container">
-    <div class="col-12">
-        <p class="title text-center py-5 display-4">Административная панель</p>
+    <div class="row">
+        <div class="col-12">
+            <p class="text-center py-2 text-white my-4 display-4 bg-dark">Административная панель</p>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <p class="py-5 display-4">Настройки</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-6 col-md-4 pb-3">
+            <a href="/admin/user" class="text-dark">Пользователи</a>
+        </div>
     </div>
 </div>
