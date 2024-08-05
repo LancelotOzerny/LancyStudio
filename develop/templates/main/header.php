a4f0fc4b4c10d24a2dad1cc78886be0228114a17
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-08-05 03:32:15 +0300

    Добавил шаблон главной страницы

---
+++ develop/templates/main/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+
+    <link rel="stylesheet" href="/assets/css/bootstrap-4.4.1.css">
+    <link rel="stylesheet" href="/assets/css/style.css">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom position-sticky w-100">
+    <div class="container d-flex justify-content-between">
+        <a class="navbar-brand" href="#">
+            <img src="/assets/images/logo.png" alt="Lancy" width="64">
+        </a>
+        <div class="collapse navbar-collapse">
+            <div class="navbar-nav">
+                <a class="nav-link active" aria-current="page" href="/">Главная</a>
+                <a class="nav-link" href="/portfolio">Портфолио</a>
+                <a class="nav-link" href="/certificates">Сертификаты</a>
+                <a class="nav-link" href="/blog">Блог</a>
+            </div>
+        </div>
+        <a class="profile d-flex align-items-center text-dark" href="#">
+            <div class="mr-2">Вход/Регистрация</div>
+            <img src="/assets/images/empty_profile_icon.png" alt="profile" width="64">
+        </a>
+    </div>
+</nav>
