c9c9c936383299b4c553b9ee7049b2e82595bab4
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-08-06 00:16:14 +0300

    Внес правки в верстку страницы 404

--- develop/views/404.php
+++ develop/views/404.php
@@ -1,17 +1,21 @@
-<div class="container">
-    <div class="row">
-        <div class="col-12 text-center">
-            <p class="display-4 py-5">Страница 404</p>
+<div class="page-block">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+                <p class="display-4">Страница 404</p>
+            </div>
         </div>
     </div>
 </div>
 
-<div class="container">
-    <div class="row">
-        <div class="col-12 text-center">
-            <p>К сожалению, вы перешли на несуществующую страницу.</p>
-            <p>Попробуйте перейти по другой, к примеру, по ссылке из главного меню!</p>
-            <p>С уважением, администрация сайта.</p>
+<div class="page-block">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+                <p>К сожалению, вы перешли на несуществующую страницу.</p>
+                <p>Попробуйте перейти по другой, к примеру, по ссылке из главного меню!</p>
+                <p>С уважением, администрация сайта.</p>
+            </div>
         </div>
     </div>
 </div>
