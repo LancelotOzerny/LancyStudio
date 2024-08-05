37c7633e432df0b9633c64570b2fa49180b4a1d6
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-08-05 22:59:44 +0300

    Сверстал раздел портфолио

--- develop/views/main/portfolio.php
+++ develop/views/main/portfolio.php
@@ -7,5 +7,22 @@
                 </div>
             </div>
         </div>
+
+        <div class="row pt-md-5">
+            <?php for ($i = 0; $i < 8; ++$i): ?>
+            <div class="col-12 col-sm-6 col-lg-4">
+                <div class="project-wrapper overflow-hidden position-relative mb-4">
+                    <img class="w-100" src="/templates/main/assets/images/no_image.png" alt="test">
+                    <div class="project-info position-absolute">
+                        <p class="text-center">The causen</p>
+                        <p class="project-info-description">Игровое приложение, в котором вам предстоит пройти удивительное приключение</p>
+                        <div class="d-flex justify-content-center">
+                            <a class="mx-auto" href="#">Перейти</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <?php endfor; ?>
+        </div>
     </div>
 </div>
