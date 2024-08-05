fe095d86d214aa23d25038fbb14845160de886f7
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-08-05 12:17:48 +0300

    Поправил доступ к массиву

--- core/config/App.php
+++ core/config/App.php
@@ -6,7 +6,7 @@ use Core\Traits\Singleton;
 class App
 {
     use Singleton;
-    private array $database = [
+    public array $database = [
         'host' => 'localhost',
         'database' => '',
         'user' => 'root',
