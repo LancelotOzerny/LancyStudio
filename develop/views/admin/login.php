3b4dc95dd5a9b1bf66c42ae26ae31217752a80e1
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-08-06 00:16:25 +0300

    Сверстал страницу входа

--- develop/views/admin/login.php
+++ develop/views/admin/login.php
@@ -1,2 +1,24 @@
-<?php
-echo 'login';
+<div class="container">
+    <div class="row">
+        <div class="col-12 d-flex flex-column justify-content-center align-items-center" style="height: 100vh">
+            <div class="wrapper">
+                <p class="pb-4 display-4">Авторизация</p>
+                <form style="width: 300px">
+                    <div class="mb-3">
+                        <label for="InputLogin" class="form-label">Логин</label>
+                        <input type="text" class="form-control" id="InputLogin">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="InputPassword" class="form-label">Пароль</label>
+                        <input type="password" class="form-control" id="InputPassword">
+                    </div>
+
+                    <div class="d-flex justify-content-center pt-4">
+                        <button id="AuthButon" class="btn btn-success text-uppercase">войти</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
