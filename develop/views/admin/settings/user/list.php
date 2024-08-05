15336e2ab29339e623f23cb2ef8ab82fc8bf687c
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-08-05 12:13:54 +0300

    Сверстал страницу со списком пользователей

--- develop/views/admin/settings/user/list.php
+++ develop/views/admin/settings/user/list.php
@@ -1,2 +1,49 @@
-<?php
-echo 'User List';
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <p class="text-center py-2 text-white my-4 display-4 bg-dark">Список пользователей</p>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <table class="table table-hover border-bottom">
+                <thead>
+                    <tr>
+                        <th><input type="checkbox"></th>
+                        <th>ID</th>
+                        <th>Логин</th>
+                        <th>Email</th>
+                        <th>Права</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><input type="checkbox"></td>
+                        <th>0</th>
+                        <td>Lancelot</td>
+                        <td>lancelot.ozernuy@gmail.com</td>
+                        <td>admin</td>
+                        <td class='text-right'>
+                            <a href="#" class="btn btn-danger">Удалить</a>
+                            <a href="#" class="btn btn-secondary">Изменить</a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <div class="list-inline-item d-flex justify-content-end">
+                <a href="/user/create" class="btn btn-success btn-sm rounded-1 text-uppercase" title="Create">Создать</a>
+            </div>
+        </div>
+    </div>
+</div>
