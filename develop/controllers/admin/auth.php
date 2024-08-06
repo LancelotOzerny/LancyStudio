3976b7deb8da46d061fe609dfa21d0da689a06e8
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-08-06 16:42:57 +0300

    Добавил действие для проверки данных пользователя

--- develop/controllers/admin/auth.php
+++ develop/controllers/admin/auth.php
@@ -3,9 +3,57 @@
 namespace Develop\Controllers\Admin;
 
 use Core\Classes\Controller;
+use Core\Classes\Request;
+use Develop\Models\Admin\Settings\User_Model;
 
 class Auth extends Controller
 {
+    public function auth()
+    {
+        $errors = [];
+
+        if (Request::Instance()->isPost('user-login') === false)
+        {
+            $errors[] = 'Не указан логин';
+        }
+        if (Request::Instance()->isPost('user-password') === false)
+        {
+            $errors[] = 'Не указан пароль';
+        }
+
+        if (count($errors) > 0)
+        {
+            return $errors;
+        }
+
+        $login = Request::Instance()->getPost('user-login');
+        $password = Request::Instance()->getPost('user-password');
+
+        $login = htmlspecialchars(trim($login));
+        $password = htmlspecialchars(trim($password));
+
+        $model = new User_Model();
+        $model->select(['id', 'login', 'password']);
+        $model->where(['email' => $login]);
+
+        $userArr = $model->getFirst();
+        if (count($userArr) > 0)
+        {
+            if (password_verify($password, $userArr['password']))
+            {
+                $_SESSION['current-user-id'] = $userArr['id'];
+            }
+            else
+            {
+                $errors[] = 'Неверный пароль!';
+            }
+        }
+        else
+        {
+            $errors[] = 'Пользователь с таким email не найден!';
+        }
+        echo json_encode($errors);
+    }
     public function login()
     {
         $this->view('admin/login', 'admin');
