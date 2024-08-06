92bc1a1c907f14b9342aee20d5de3f73661cc974
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-08-06 18:34:18 +0300

    Реализовал установку уровня доступа в маршрутах

--- core/classes/router.php
+++ core/classes/router.php
@@ -5,12 +5,14 @@ class Router
 {
     private $routesList = [];
 
-    public function add(string $from, string $to, int $levelRight = 0)
+    public function add(string $from, string $to, int $levelAccess = 0, string $toPage = '/')
     {
         [$controller, $action] = explode('::', $to);
 
         $this->routesList[] = [
             'from' => $from,
+            'access_level' => $levelAccess,
+            'redirect_page' => $toPage,
             'controller' => $controller,
             'action' => $action,
         ];
@@ -24,6 +26,12 @@ class Router
         {
             if ($route['from'] === $url)
             {
+                $access = $this->isAccessAllowed($route['access_level']);
+                if ($access === false)
+                {
+                    header('Location:' . $route['redirect_page']);
+                }
+
                 $included = $this->includeFileIfExists($route['controller'], $route['action']);
                 if ($included)
                 {
@@ -36,6 +44,19 @@ class Router
         {
             echo 'Page 404';
         }
+    }
+
+    private function isAccessAllowed(int $accessLevel)
+    {
+        if ($accessLevel > 0)
+        {
+            if (USER->rightsLevel < $accessLevel)
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     private function includeFileIfExists(string $controller, string $action)
--- core/config/routes.php
+++ core/config/routes.php
@@ -1,14 +1,18 @@
 <?php
 /** @var \Core\Classes\Router $router */
 
-$router->add('/admin/settings/user', 'Admin/User::list', 100);
-$router->add('/admin/settings/rights', 'Admin/Rights::list', 100);
+/* -------------------- ADMIN ROUTES -------------------- */
+$loginPage = '/admin/login';
 
-$router->add('/admin/login', 'Admin/Auth::login');
+$router->add('/admin/settings/user', 'Admin/User::list', 100, $loginPage);
+$router->add('/admin/settings/rights', 'Admin/Rights::list', 100, $loginPage);
+$router->add('/admin', 'Admin/Home::index', 100, $loginPage);
+
+$router->add($loginPage, 'Admin/Auth::login');
 $router->add('/admin/auth', 'Admin/Auth::auth');
 
-$router->add('/admin', 'Admin/Home::index', 100);
 
+/* -------------------- PUBLIC ROUTES -------------------- */
 $router->add('/', 'Main::index');
 $router->add('/blog', 'Main::blog');
 $router->add('/portfolio', 'Main::portfolio');
