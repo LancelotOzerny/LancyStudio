e4cd5aab30d99925e06e597157ce22253b8b351f
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-08-06 16:39:53 +0300

    Реализовал инициализацию вошедшего пользователя

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -1,10 +1,21 @@
 <?php
+session_start();
+
 /** @const string HOME_PATH */
 require_once HOME_PATH . '/core/config/autoload.php';
 
 use Core\Classes\Router;
+use Core\Classes\User;
 
 $router = new Router();
 require_once HOME_PATH . '/core/config/routes.php';
+
+\Core\Classes\Request::Instance()->init();
+
+$user = null;
+if (isset($_SESSION['current-user-id']))
+{
+    $user = new User(intval($_SESSION['current-user-id']));
+}
 
 $router->start();
