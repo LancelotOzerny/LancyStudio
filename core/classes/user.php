1edb5c3bffab0628db92ee8c334125e071a8c3f2
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-08-06 16:39:22 +0300

    Добавил класс пользователя

---
+++ core/classes/user.php
@@ -0,0 +1,26 @@
+<?php
+namespace  Core\Classes;
+
+use Develop\Models\Admin\Settings\User_Model;
+
+class User
+{
+    public int $rightsLevel = 0;
+    public string $login = '';
+    public string $email = '';
+    public string $rightsName = '';
+
+    public function __construct(int $id = 0)
+    {
+        $model = new User_Model();
+        $model->select(['users.id', 'users.login', 'users.email', 'rights.name', 'rights.level']);
+        $model->join('rights', 'users.rights_id', 'rights.id');
+
+        $userArr = $model->getFirst();
+
+        $this->login = $userArr['login'];
+        $this->rightsLevel = $userArr['level'];
+        $this->email = $userArr['email'];
+        $this->rightsName = $userArr['name'];
+    }
+}
